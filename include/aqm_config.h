bd4aee25fc8fd00e71408b972d07fa28fe85f0d2
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-02-18 19:21:35 +0100

    pinout file + some configs

---
+++ include/aqm_config.h
@@ -0,0 +1,31 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+
+#define LED_PIN 2
+
+#define BOOT_BUTTON_PIN 46
+
+#define RS485_D_PIN 9
+#define RS485_DE_PIN 10
+#define RS485_R_PIN 11
+
+#define RELAY_PIN 12
+
+#define I2C_SDA_PIN 47
+#define I2C_SCL_PIN 21
+
+#define ADC_1_RDY_PIN 14
+#define ADC_2_RDY_PIN 13
+
+#define SEN55_ADDRESS 0x69
+#define ADS1115_1_ADDRESS 0x48
+#define ADS1115_2_ADDRESS 0x49
+
+#define AQM_WIFI_SSID      "IoT"
+#define AQM_WIFI_PASS      "AQM_Project"
+#define AQM_MAX_RETRY      5
+
+
+
+#endif // PINOUT_H
