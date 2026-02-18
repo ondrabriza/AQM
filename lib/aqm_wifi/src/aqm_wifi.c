e0f94a06fe78db7e14ef83173c23d518c25f66b9
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-02-18 19:26:35 +0100

    wifi connect

---
+++ lib/aqm_wifi/src/aqm_wifi.c
@@ -0,0 +1,57 @@
+#include "aqm_wifi.h"
+#include "aqm_config.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_netif.h>
+
+
+static const char *TAG = "AQM_WIFI";
+static esp_netif_t *s_sta_netif = NULL;
+
+/**
+ * @brief Event handler for Wi-Fi and IP events
+ */
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Got IP address: " IPSTR, IP2STR(&event->ip_info.ip));
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGW(TAG, "Wi-Fi disconnected, retrying...");
+        esp_wifi_connect();
+    }
+}
+
+void aqm_wifi_connect(void) {
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    
+    ESP_ERROR_CHECK(esp_netif_init());
+    s_sta_netif = esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = AQM_WIFI_SSID,
+            .password = AQM_WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_ERROR_CHECK(esp_wifi_set_max_tx_power(8));
+
+
+
+}
--- lib/aqm_wifi/src/aqm_wifi.h
+++ lib/aqm_wifi/src/aqm_wifi.h
@@ -1,4 +1,11 @@
 #ifndef AQM_WIFI_H
 #define AQM_WIFI_H
 
+
+/**
+ * @brief Initialize Wi-Fi in station mode and connect to the Access Point.
+ * * @return esp_err_t ESP_OK on success
+ */
+void aqm_wifi_connect(void);
+
 #endif // AQM_WIFI_H
