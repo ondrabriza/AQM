dd33103565f18dbf61960c4568204a936ff17b2b
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-03-30 16:21:56 +0200

    added relay functions

--- lib/aqm_gpio/src/aqm_gpio.h
+++ lib/aqm_gpio/src/aqm_gpio.h
@@ -14,4 +14,10 @@ esp_err_t aqm_led_turn_off(void);
 
 esp_err_t aqm_led_toggle(void);
 
+esp_err_t aqm_relay_toggle(void);
+
+uint8_t aqm_get_led_state(void);
+
+uint8_t aqm_get_relay_state(void);
+
 #endif // AQM_GPIO_H
