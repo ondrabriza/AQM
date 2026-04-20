84ba4568de1ec15ab119952142facbe15fbfc5a2
ondrabriza <109350911+ondrabriza@users.noreply.github.com>
2026-04-20 17:35:10 +0200

    Refactor MICS baseline calculations to use unified ratio checks and alpha value

--- lib/aqm_mics6814/src/aqm_mics6814.c
+++ lib/aqm_mics6814/src/aqm_mics6814.c
@@ -3,9 +3,9 @@
 #include "aqm_config.h"
 #include "ads1115.h"
 
-
-#define ALPHA_CLEANING   0.005f   
-#define ALPHA_POLLUTING  0.0005f
+#define RATIO_MIN 0.95f
+#define RATIO_MAX 1.05f
+#define ALPHA 0.95f
 
 void calculate_mics_r(uint16_t mics_adc_raw, ads1115_pga_t pga, uint16_t voltage_in_mv, uint32_t r_load, uint32_t *out_r) {
     
@@ -45,22 +45,18 @@ void aqm_calculate_data_from_mics(void){
 void aqm_update_mics_r0_baselines(void) {
 
     // RED: Clean air = HIGH Resistance. Gas detection = DECREASE in resistance.
-    if(aqm_data.data.gases.mics_red > aqm_data.config.mics_thresholds.red_threshold) {
-        float alpha_red = (aqm_data.data.gases.mics_red_r > aqm_data.config.mics_r0.red_r0) ? ALPHA_CLEANING : ALPHA_POLLUTING;
-        aqm_data.config.mics_r0.red_r0 = (uint32_t)((aqm_data.data.gases.mics_red_r * alpha_red) + (aqm_data.config.mics_r0.red_r0 * (1.0f - alpha_red)) + 0.5f); // +0.5f for rounding
+    if(aqm_data.data.gases.mics_red >= RATIO_MIN && aqm_data.data.gases.mics_red <= RATIO_MAX) {
+        aqm_data.config.mics_r0.red_r0 = (uint32_t)((aqm_data.data.gases.mics_red_r * ALPHA) + (aqm_data.config.mics_r0.red_r0 * (1.0f - ALPHA)) + 0.5f); // +0.5f for rounding
     }
     
     // NH3: Clean air = HIGH Resistance. Gas detection = DECREASE in resistance.
-    if(aqm_data.data.gases.mics_nh3 > aqm_data.config.mics_thresholds.nh3_threshold) {
-        float alpha_nh3 = (aqm_data.data.gases.mics_nh3_r > aqm_data.config.mics_r0.nh3_r0) ? ALPHA_CLEANING : ALPHA_POLLUTING;
-        aqm_data.config.mics_r0.nh3_r0 = (uint32_t)((aqm_data.data.gases.mics_nh3_r * alpha_nh3) + (aqm_data.config.mics_r0.nh3_r0 * (1.0f - alpha_nh3)) + 0.5f);
+    if(aqm_data.data.gases.mics_nh3 >= RATIO_MIN && aqm_data.data.gases.mics_nh3 <= RATIO_MAX){
+        aqm_data.config.mics_r0.nh3_r0 = (uint32_t)((aqm_data.data.gases.mics_nh3_r * ALPHA) + (aqm_data.config.mics_r0.nh3_r0 * (1.0f - ALPHA)) + 0.5f);
     }
 
     // OX: Clean air = LOW Resistance. Gas detection = INCREASE in resistance.
-    if(aqm_data.data.gases.mics_ox < aqm_data.config.mics_thresholds.ox_threshold) {
-    
-        float alpha_ox = (aqm_data.data.gases.mics_ox_r < aqm_data.config.mics_r0.ox_r0) ? ALPHA_CLEANING : ALPHA_POLLUTING;
-        aqm_data.config.mics_r0.ox_r0 = (uint32_t)((aqm_data.data.gases.mics_ox_r * alpha_ox) + (aqm_data.config.mics_r0.ox_r0 * (1.0f - alpha_ox)) + 0.5f);
+    if(aqm_data.data.gases.mics_ox >= RATIO_MIN && aqm_data.data.gases.mics_ox <= RATIO_MAX) {
+        aqm_data.config.mics_r0.ox_r0 = (uint32_t)((aqm_data.data.gases.mics_ox_r * ALPHA) + (aqm_data.config.mics_r0.ox_r0 * (1.0f - ALPHA)) + 0.5f);
     }
 
 }
